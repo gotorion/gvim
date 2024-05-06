98e6e6d2e16b2d6ba5a1290f0ecabad310cac1f4
Junhui Li <junhui.lee@icloud.com>
2024-05-06 22:31:08 +0800

    Update plugins

--- init.lua
+++ init.lua
@@ -1,3 +1,24 @@
+-- install lazyvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    "scottmckendry/cyberdream.nvim",
+    "nvim-lualine/lualine.nvim",
+    dependecied = { 'nvim-tree/nvim-web-devicons' }
+})
+
+
 require('plugins')
 require('options')
 require('keymaps')
