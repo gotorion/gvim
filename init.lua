d608392277e28f9988732ef5430bf1cf608ed1b9
Junhui Li <junhui.lee@icloud.com>
2024-08-04 10:15:14 +0800

    add markdown plugin

--- init.lua
+++ init.lua
@@ -15,11 +15,62 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     "scottmckendry/cyberdream.nvim",
     "nvim-lualine/lualine.nvim",
+    "neovim/nvim-lspconfig",
+    "ms-jpq/coq_nvim",
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
+    },
     dependecied = { 'nvim-tree/nvim-web-devicons' }
 })
 
+require'lspconfig'.clangd.setup{
+    cmd = {
+      "clangd",
+      "--background-index",
+      "--clang-tidy",
+      "--header-insertion=iwyu",
+      "--completion-style=detailed",
+      "-j=4",
+      "--fallback-style=LLVM",
+    },
+}
 
 require('plugins')
 require('options')
 require('keymaps')
 require('colorscheme')
+
+return {
+{
+  "neovim/nvim-lspconfig", -- REQUIRED: for native Neovim LSP integration
+  lazy = false, -- REQUIRED: tell lazy.nvim to start this plugin at startup
+  dependencies = {
+    -- main one
+    { "ms-jpq/coq_nvim", branch = "coq" },
+
+    -- 9000+ Snippets
+    { "ms-jpq/coq.artifacts", branch = "artifacts" },
+
+    -- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
+    -- Need to **configure separately**
+    { 'ms-jpq/coq.thirdparty', branch = "3p" }
+    -- - shell repl
+    -- - nvim lua api
+    -- - scientific calculator
+    -- - comment banner
+    -- - etc
+  },
+  init = function()
+    vim.g.coq_settings = {
+        auto_start = true, -- if you want to start COQ at startup
+        -- Your COQ settings here
+    }
+  end,
+  config = function()
+    -- Your LSP settings here
+  end,
+}
+}
