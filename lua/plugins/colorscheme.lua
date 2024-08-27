1e11858eec6c116dcc6e176cccca59a2b1e91618
Junhui Li <junhui.lee@icloud.com>
2024-08-27 09:59:10 +0800

    move colorscheme to plugins folder

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+-- define colorscheme here
+return {
+    {
+            "folke/tokyonight.nvim",
+            config = function()
+                vim.cmd('colorscheme tokyonight')
+            end
+    },
+}
+-- require('tokyonight').load()
+
--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,11 +1,17 @@
 return {
-    "navarasu/onedark.nvim",
-    "nvim-lualine/lualine.nvim",
     {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        ft = { "markdown" },
-        build = function() vim.fn["mkdp#util#install"]() end,
+        {
+            "iamcco/markdown-preview.nvim",
+            cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+            ft = { "markdown" },
+            build = function() vim.fn["mkdp#util#install"]() end,
+        },
+        {
+            'MeanderingProgrammer/render-markdown.nvim',
+                opts = {},
+                -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+                -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+                dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+        },
     },
-    dependecied = { 'nvim-tree/nvim-web-devicons' }
 }
