996850ed62f87bc6d975781da4d15406b0998f89
Junhui Li <junhui.lee@icloud.com>
2024-08-31 17:52:03 +0800

    update: lualine & terminal

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -6,8 +6,9 @@ return {
                   options = {
                     icons_enabled = true,
                     theme = 'auto',
-                    component_separators = { left = '', right = ''},
-                    section_separators = { left = '', right = ''},
+                    component_separators = { left = '|', right = '|'},
+                    section_separators = { left = ' ', right = ' '},
+                    -- section_separators = { left = '', right = ''},
                     disabled_filetypes = {
                       statusline = {},
                       winbar = {},
