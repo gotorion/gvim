ad4fb9a0775c8e8e78d4fbbd2eb8a9e397753948
Junhui Li <junhui.lee@icloud.com>
2024-09-22 15:30:13 +0800

    update: plugins

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,22 +3,32 @@ return {
     dependencies = { "nvim-lua/plenary.nvim", },
     config = function()
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-        vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
-        vim.keymap.set('n', '<leader>fc', builtin.lsp_incoming_calls, {})
-        vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
-        vim.keymap.set('n', '<leader>fe', builtin.diagnostics, {})
-        vim.keymap.set('n', '<leader>fd', builtin.lsp_definitions, {})
-        vim.keymap.set('n', '<leader>fi', builtin.lsp_implementations, {})
+        local wk = require('which-key')
+        wk.add({
+            { "<leader>ff", function() builtin.find_files() end, desc = "find file" },
+            { '<leader>fg', function() builtin.live_grep() end, desc = "find grep" },
+            { '<leader>fb', function() builtin.buffers() end, desc = "find buffer" },
+            { '<leader>fh', function() builtin.help_tags() end, desc = "find help" },
+            { '<leader>fr', function() builtin.lsp_references() end, desc = "find reference" },
+            { '<leader>fc', function() builtin.lsp_incoming_calls() end, desc = "find incomming calls" },
+            { '<leader>fs', function() builtin.lsp_document_symbols() end },
+            { '<leader>fe', function() builtin.diagnostics() end, desc = "find diagnostics" },
+            { '<leader>fd', function() builtin.lsp_definitions() end, desc = "find definitions" },
+            { '<leader>fi', function() builtin.lsp_implementations() end, desc = "find implementations" },
+            { '<leader>fm', function() builtin.man_pages() end, desc = "find man pages" },
+            { '<leader>f,', "<cmd>Telescope git_commits<CR>" },
+            { '<leader>fb', "<cmd>Telescope git_branches<CR>" },
+        })
+
         require('telescope').setup {
             pickers = {
                 file_files = {
                     theme = 'cursor'
                 },
-            }
+            },
+            mappings = {
+                n = {  ["<esc>"] = require('telescope.actions').close, },
+            },
         }
     end
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,92 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    opts = {
-        ensure_installed = {"c", "lua", "markdown", "cmake", "cpp"},
+    config = function()
+        require('nvim-treesitter.configs').setup {
+        ensure_installed = {"c", "cpp", "cuda", "cmake", "lua", "python", "html", "javascript", "css", "json", "bash", "regex", "markdown", "diff", "glsl", "vim", "vimdoc"},
         highlight = {
             enable = true,
-            additional_vim_regex_highlighting = true,
+            additional_vim_regex_highlighting = false,
         },
-        auto_install = true,
-    },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = '+',
+                node_incremental = '+',
+                node_decremental = '-',
+                -- scope_incremental = '+',
+            }
+        },
+        indent = { enable = true, },
+        rainbow = {
+            enable = true,
+            extended_mode = true,
+        },
+        matchup = { enable = true, },
+        context_commentstring = { enable = true, },
+        autotag = { enable = true, },
+        textobjects = {
+            select = {
+                enable = true,
+                -- Automatically jump forward to textobj, similar to targets.vim
+                lookahead = true,
+                keymaps = {
+                    -- You can use the capture groups defined in textobjects.scm
+                    ["af"] = "@function.outer",
+                    ["if"] = "@function.inner",
+                    ["ac"] = "@class.outer",
+                    ["ic"] = "@class.inner",
+                    ["as"] = { query = "@scope", query_group = "locals" },
+                    ["az"] = { query = "@fold", query_group = "folds" },
+                    ["ai"] = "@call.outer",
+                    ["ii"] = "@call.inner",
+                    ["ad"] = "@conditional.outer",
+                    ["id"] = "@conditional.inner",
+                    ["ae"] = "@loop.outer",
+                    ["ie"] = "@loop.inner",
+                    ["ap"] = "@parameter.outer",
+                    ["ip"] = "@parameter.inner",
+                    ["ab"] = "@block.outer",
+                    ["ib"] = "@block.inner",
+                    ["at"] = "@comment.outer",
+                    ["it"] = "@comment.inner",
+                    ["ar"] = "@return.outer",
+                    ["ir"] = "@return.inner",
+                    ["al"] = "@statement.outer",
+                    ["il"] = "@statement.inner",
+                    ["an"] = "@number.outer",
+                    ["in"] = "@number.inner",
+                    ["ah"] = "@assignment.outer",
+                    ["ih"] = "@assignment.inner",
+                },
+                -- You can choose the select mode (default is charwise 'v')
+                --
+                -- Can also be a function which gets passed a table with the keys
+                -- * query_string: eg '@function.inner'
+                -- * method: eg 'v' or 'o'
+                -- and should return the mode ('v', 'V', or '<c-v>') or a table
+                -- mapping query_strings to modes.
+                selection_modes = {
+                    ['@parameter.outer'] = 'v', -- charwise
+                    ['@function.outer'] = 'V', -- linewise
+                    ['@statement.outer'] = 'V', -- linewise
+                    ['@assignment.outer'] = 'V', -- linewise
+                    ['@block.outer'] = 'V', -- linewise
+                    ['@loop.outer'] = 'V', -- linewise
+                    ['@conditional.outer'] = 'V', -- linewise
+                    ['@class.outer'] = 'V', -- linewise
+                },
+                -- If you set this to `true` (default is `false`) then any textobject is
+                -- extended to include preceding or succeeding whitespace. Succeeding
+                -- whitespace has priority in order to act similarly to eg the built-in
+                -- `ap`.
+                --
+                -- Can also be a function which gets passed a table with the keys
+                -- * query_string: eg '@function.inner'
+                -- * selection_mode: eg 'v'
+                -- and should return true of false
+                include_surrounding_whitespace = false,
+            },
+        },
+    }
+    end
 }
