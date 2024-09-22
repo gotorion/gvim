8251654420b98d66b8c123c8bacc748c2f85d9aa
Junhui Li <junhui.lee@icloud.com>
2024-09-22 14:04:16 +0800

    add: indent plugin

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -2,6 +2,11 @@ return {
     "mfussenegger/nvim-dap",
     config = function()
         local dap = require("dap")
+        dap.adapters.gdb = {
+          type = "executable",
+          command = "gdb",
+          args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
+        }
         dap.configurations.cpp = {
           {
             name = "Launch",
