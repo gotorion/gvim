01f82cc2c881bf8c0b63c34df03aa1e75cac8f48
qcloud <ubuntu@localhost.localdomain>
2024-11-14 15:35:51 +0800

    update colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
 local conf = {
-    -- "polirritmico/monokai-nightasty.nvim",
-    "miikanissi/modus-themes.nvim",
+    "polirritmico/monokai-nightasty.nvim",
+    -- "miikanissi/modus-themes.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-        vim.cmd([[colorscheme modus_operandi]])
-        -- require("monokai-nightasty").setup{}
-        -- require("monokai-nightasty").load{}
+        -- vim.cmd([[colorscheme modus_operandi]])
+        require("monokai-nightasty").setup{}
+        require("monokai-nightasty").load{}
     end
 }
 return conf
