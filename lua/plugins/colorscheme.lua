c1ea188eb25f03b314d4290bb7e31b34b7eb5bf7
Junhui Li <junhui.lee@icloud.com>
2024-11-18 10:44:08 +0800

    update colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,26 @@
+-- local conf = {
+--     "polirritmico/monokai-nightasty.nvim",
+--     -- "miikanissi/modus-themes.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--         -- vim.cmd([[colorscheme modus_operandi]])
+--         require("monokai-nightasty").setup{}
+--         require("monokai-nightasty").load{}
+--     end
+-- }
+
 local conf = {
-    "polirritmico/monokai-nightasty.nvim",
-    -- "miikanissi/modus-themes.nvim",
+    "Mofiqul/vscode.nvim",
     lazy = false,
     priority = 1000,
-    config = function()
-        -- vim.cmd([[colorscheme modus_operandi]])
-        require("monokai-nightasty").setup{}
-        require("monokai-nightasty").load{}
+    config =function()
+        vim.o.background = 'light'
+        local c = require('vscode.colors').get_colors()
+        require('vscode').setup {
+            italic_comments = true,
+        }
+        vim.cmd([[colorscheme vscode]])
     end
 }
 return conf
