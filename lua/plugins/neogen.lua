5023a799a9b9c2c9cd53c53c2adc1b4b9586e60a
Junhui Li <junhui.lee@icloud.com>
2024-11-25 10:47:49 +0800

    add neogen keybinding

--- lua/plugins/neogen.lua
+++ lua/plugins/neogen.lua
@@ -1,8 +1,11 @@
 return {
     'danymat/neogen',
-    config = function()
-        require('neogen').setup {
-
-        }
+    config = function ()
+        local opts = { noremap = true, silent = true}
+        vim.api.nvim_set_keymap('n', '<leader>nf', ":lua require('neogen').generate()<CR>", opts)
+        require('neogen').setup({
+            enabled = true,
+            snippet_engine = 'luasnip',
+        })
     end
 }
